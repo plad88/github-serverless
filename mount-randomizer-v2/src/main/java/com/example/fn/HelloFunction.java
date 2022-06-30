868741a9174872c8d617fba1edb5a8bcb6bdee9f
Gomez Cornejo Gil, Francisco Javier <fjgcornejo@minsait.com>
2022-06-30 13:42:18 +0200

    update

--- mount-randomizer-v2/src/main/java/com/example/fn/HelloFunction.java
+++ mount-randomizer-v2/src/main/java/com/example/fn/HelloFunction.java
@@ -5,11 +5,12 @@ import java.util.Random;
 public class HelloFunction {
 
 	public String handleRequest(String input) {
-		final Integer total = (input == null || input.isEmpty()) ? 1000 : Integer.valueOf(input);
+		final Integer total = input == null || input.isEmpty() ? 1000 : Integer.valueOf(input);
 		final int drop = 17;
 
 		boolean found = false;
 		int iterations = 0;
+		boolean lucky = false;
 
 		while (!found) {
 			final Integer guessed = new Random().nextInt(total);
@@ -21,9 +22,16 @@ public class HelloFunction {
 			}
 			iterations++;
 		}
+		if (iterations < 190) {
+			lucky = true;
+		}
 
 		System.out.println("Mount drop after " + iterations + " tries.");
-		return "Mount drop after " + iterations + " tries, with special number being: " + drop;
+		String output = "Mount drop after " + iterations + " tries, with special number being: " + drop;
+		if (lucky) {
+			output = output + " ...and you are lucky!!!";
+		}
+		return output;
 	}
 
 }
