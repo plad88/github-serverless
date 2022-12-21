8939cc6fd6fa9cb00e5c5e2934e0ff53263e91df
Pedro <plantona@minsait.com>
2022-12-21 11:47:12 +0100

    add pythonchanges

--- fall-predictor/func.py
+++ fall-predictor/func.py
@@ -7,13 +7,15 @@ from fdk import response
 
 def handler(ctx, data: io.BytesIO = None):
     try:
+        answer = ""
         json_obj = json.loads(data.getvalue())
         if isinstance(json_obj, dict) and 'listName' in json_obj:
             answer = []
             for input in json_obj:
                 answer.append(random())
         else:
-            raise Exception("input object is not an array of objects:" + str(json_obj))
+            answer = "input object is not an array of objects:" + str(json_obj)
+            raise Exception(answer)
     except (Exception, ValueError) as ex:
         logging.getLogger().info('error parsing json payload: ' + str(ex))
 
