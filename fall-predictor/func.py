94c759ab839446c12681750cbb1b4476bc7e9206
Pedro <plantona@minsait.com>
2022-12-21 11:17:06 +0100

    add pythonchanges

--- fall-predictor/func.py
+++ fall-predictor/func.py
@@ -6,16 +6,19 @@ from fdk import response
 
 
 def handler(ctx, data: io.BytesIO = None):
-    name = "World"
     try:
-        body = json.loads(data.getvalue())
-        name = body.get("name")
+        json_obj = json.loads(data.getvalue())
+        if isinstance(json_obj, dict) and 'listName' in json_obj:
+            answer = []
+            for input in json_obj:
+                answer.append(random())
+        else:
+            raise Exception("input object is not an array of objects:" + str(json_obj))
     except (Exception, ValueError) as ex:
         logging.getLogger().info('error parsing json payload: ' + str(ex))
 
-    logging.getLogger().info("Inside Python Hello World function")
+    logging.getLogger().info("Inside Python ML function")
     return response.Response(
-        ctx, response_data=json.dumps(
-            {"message": "Hello {0}".format(name)}),
+        ctx, response_data=json.dumps(answer),
         headers={"Content-Type": "application/json"}
     )
