7e31ac14bc5812db6d57d88a9c0919301b999f86
Pedro <plantona@minsait.com>
2023-02-03 12:32:41 +0100

    add changes

--- fall-predictor/func.py
+++ fall-predictor/func.py
@@ -30,7 +30,7 @@ def handler(ctx, data: io.BytesIO = None):
             answer = []
             for input in json_obj:
                 #answer.append(round(random.uniform(0, 1),2))
-                answer.append(pyfunc_predictor.predict([[ 7, 0.27, 0.36, 20.7, 0.045, 45, 170, 1.001, 3, 0.45, 8.8]])[0])
+                answer.append(pyfunc_predictor.predict([[ 7, 0.27, 0.36, 20.7, 0.045, 45, 170, 1.001, 3, 0.45, 8.8]]))
         else:
             answer = "input object is not an array of objects:" + str(json_obj)
             logging.getLogger().error('error isinstance(json_obj, list):' + isinstance(json_obj, list))
