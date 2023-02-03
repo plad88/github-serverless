55249ddea16b0ee9d1226abdf2d6298f4c304f50
Pedro <plantona@minsait.com>
2023-02-03 12:40:52 +0100

    add changes

--- fall-predictor/func.py
+++ fall-predictor/func.py
@@ -24,16 +24,21 @@ initML()
 
 def handler(ctx, data: io.BytesIO = None):
     try:
+        logging.getLogger().info("Try")
         answer = []
         json_obj = json.loads(data.getvalue())
+        logging.getLogger().info("json_obj")
+        logging.getLogger().info(str(json_obj))
         if isinstance(json_obj, list):
+            logging.getLogger().info("isinstance")
             answer = []
             for input in json_obj:
+                logging.getLogger().info("for")
                 #answer.append(round(random.uniform(0, 1),2))
                 predict = pyfunc_predictor.predict([[ 7, 0.27, 0.36, 20.7, 0.045, 45, 170, 1.001, 3, 0.45, 8.8]])
                 logging.getLogger().info("prediction")
                 logging.getLogger().info(predict)
-                answer.append(predict)
+                answer.append(predict[0])
         else:
             answer = "input object is not an array of objects:" + str(json_obj)
             logging.getLogger().error('error isinstance(json_obj, list):' + isinstance(json_obj, list))
