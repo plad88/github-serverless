e6c058b2633dd1fd4a2e81b3703aafd510125d68
Pedro <plantona@minsait.com>
2023-02-20 12:02:01 +0100

    add changes

--- fall-predictor/func.py
+++ fall-predictor/func.py
@@ -10,7 +10,7 @@ import mlflow
 from fdk import response
 
 tracking_uri = "https://onesait-telco-poc.onesaitplatform.com/controlpanel/modelsmanager"
-model_uri = "onesait-platform://49832a9c2d7645caa0ef497706edef34@onesait-telco-poc.onesaitplatform.com/0/6657be3510d24fa9b8308e30d6b5fa7c/artifacts/model"
+model_uri = "onesait-platform://49832a9c2d7645caa0ef497706edef34@onesait-telco-poc.onesaitplatform.com/0/19286433c9d6483e8a027ebb76aa11ce/artifacts/model"
 global pyfunc_predictor
 
 mlflow.set_tracking_uri(tracking_uri)
@@ -29,8 +29,11 @@ def handler(ctx, data: io.BytesIO = None):
             answer = []
             for input in json_obj:
                 logging.getLogger().info("for")
+                logging.getLogger().info("input: " + str(input))
                 #answer.append(round(random.uniform(0, 1),2))
                 predict = pyfunc_predictor.predict([[ 7, 0.27, 0.36, 20.7, 0.045, 45, 170, 1.001, 3, 0.45, 8.8]])
+                predict = pyfunc_predictor.predict([[ 6.9, 0.36, 0.34, 4.2, 0.018, 57, 119 ,0.9898, 3.28 , 0.36, 12.7]])
+                
                 logging.getLogger().info("prediction")
                 logging.getLogger().info(predict)
                 answer.append(predict[0])
